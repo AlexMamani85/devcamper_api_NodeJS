eefd0acb54236e8e20a4967ef3c89fd8d82af6a5
AlexMamani85 <alex.mamani@tecsup.edu.pe>
2022-10-30 11:25:36 -0500

    CHORE: Error Handler and ErrorResponse

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,3 +1,4 @@
+const ErrorResponse = require('../utils/errorResponse');
 const Bootcamp = require('../models/Bootcamp')
 
 
@@ -24,12 +25,15 @@ exports.getBootcamp = async (req, res, next) => {
       const bootcamp = await Bootcamp.findById(req.params.id);
 
       if(!bootcamp) {
-        return res.status(400).json({success: false});
+        return next(
+          new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+        );
       }
 
       res.status(200).json({success:true, data: bootcamp});
     } catch(err){
-      res.status(400).json({success: false});
+      // res.status(400).json({success: false});
+      next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
     }
   }
 
@@ -51,7 +55,7 @@ exports.createBootcamp = async(req, res, next) => {
 // @desc    Update Bootcamp
 // @route   PUT /api/v1/bootcamps
 // @access  Private
-exports.updateBootcamp = (req, res, next) => {
+exports.updateBootcamp = async(req, res, next) => {
   try{
     const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
